40eff55f71ab253c63ab5e0ca16b01e5d8b9a25c
Amanda Morris <amandahenry2@comcast.net>
2020-08-10 20:10:57 -0600

    final changes involving the full CRUD

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,12 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/styles.css",
+  "/index.js",
   "/manifest.webmanifest",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+  "/indexedDb.js"
 ];
 
 const CACHE_NAME = "static-cache-v2";
